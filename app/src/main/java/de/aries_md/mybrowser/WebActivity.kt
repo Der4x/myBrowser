9d36a11b0929ad88370fc8dafdd8c7e06ecd0b46
Alexander Aries <alexander.aries@maindreieck.com>
2018-03-30 18:53:21 +0200

    init commit

---
+++ app/src/main/java/de/aries_md/mybrowser/WebActivity.kt
@@ -0,0 +1,12 @@
+package de.aries_md.mybrowser
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WebActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+    }
+}
